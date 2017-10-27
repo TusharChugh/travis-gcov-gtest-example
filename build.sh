615629ab3c6ac5bc27165d3a96ed7d02e61b73a8
Tushar Chugh <techie2sh@gmail.com>
2017-10-26 22:57:03 -0400

    got gcov working yaygit push

--- build.sh
+++ build.sh
@@ -3,5 +3,6 @@ mkdir -p build
 cd build
 cmake -DCMAKE_BUILD_TYPE=ON -DBUILD_TESTS=ON .. && make
 make test
+make travis_gcov_example_coverage
 cd ..
 codecov -t 14941eb5-212f-45d2-8f93-b00e31f7ea8f -X gcov
